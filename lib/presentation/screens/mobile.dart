db74e67b7bafdb16daffccd17cf476d94b2db4dd
rohitshelar23 <shelar.rohit2332@gmail.com>
2026-08-29 13:03:16 +0530

    Small improvement in mobile.dart

--- lib/presentation/screens/mobile.dart
+++ lib/presentation/screens/mobile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../cubit/complaint_cubit.dart';
 import '../cubit/complaint_state.dart';
 import '../widgets/complaint_card.dart';
@@ -7,39 +8,45 @@ import '../widgets/complaint_card.dart';
 class MobileScreen extends StatelessWidget {
   const MobileScreen({super.key});
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Complaints'),
+        title: const Text('Complaints'),
       ),
-      body:BlocBuilder<ComplaintCubit, ComplaintState>
-      (builder: (context,state) {
-        if (state is ComplaintLoading) {
-          return const Center(child: CircularProgressIndicator());
-        } 
+      body: BlocBuilder<ComplaintCubit, ComplaintState>(
+        builder: (context, state) {
+          if (state is ComplaintLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-        if(state is ComplaintError){
-          return Center(child: Text(state.message));
-        }
-        
-        if (state is ComplaintLoaded) {
-          return ListView.builder(
-            padding: const EdgeInsets.all(16),
-            itemCount: state.complaints.length,
-            itemBuilder: (context, index) {
-              final complaint = state.complaints[index];
-             
-             return ComplaintCard(complaint: complaint);
-            },
-          );   
-        }
+          if (state is ComplaintError) {
+            return Center(
+              child: Text(state.message),
+            );
+          }
 
-          return const Center( child: Text('No complaints.'),
+          if (state is ComplaintLoaded) {
+            return ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: state.complaints.length,
+              itemBuilder: (context, index) {
+                final complaint = state.complaints[index];
+
+                return ComplaintCard(
+                  complaint: complaint,
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: Text('No complaints found'),
           );
-        }
+        },
       ),
-     );
-   }
+    );
   }
+}
